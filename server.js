9eebf8fdf0914d1c2fb1645741ab6993baf049bf
kukukaia <kukukaia@protonmail.com>
2022-05-27 11:43:27 -0300

    alterações

---
+++ server.js
@@ -0,0 +1,17 @@
+//modelo
+//Ele vai pedir o arquivo "dotenv". Se não estiver no ambiente de produção
+//pq aqui estamos no ambiente de desenvolvimento
+//então não temos controle sobre o ambiente do servidor
+
+if(process.env.NODE_ENV !=='production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.end("Servidor ativo")
+})
+
+app.listen(process.env.PORT, console.log("servidor iniciado"))
